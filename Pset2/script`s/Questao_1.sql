3c47296d5321a66bc48ac6375ef9bd7fe7c1e7af
RodrigoFass <103529923+RodrigoFass@users.noreply.github.com>
2022-05-12 20:23:18 -0300

    Create Questao_1.sql

---
+++ Pset2/script`s/Questao_1.sql
@@ -0,0 +1,4 @@
+SELECT d.numero_departamento, AVG(salario) AS media_salarial
+FROM funcionarios AS f, departamento AS d
+WHERE d.numero_departamento = f.numero_departamento
+GROUP BY d.numero_departamento;
